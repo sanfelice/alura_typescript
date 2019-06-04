47924a49583f03cf7b3a229b0cb6e13b7fa97b04
Ricardo Sanfelice <ricardo.sanfelice@vrsoft.com.br>
2019-06-03 22:28:32 -0300

    Curso Javascript MOD3 - Aula 3

--- ecmascript6/client/js/app/controllers/NegociacaoController.js
+++ ecmascript6/client/js/app/controllers/NegociacaoController.js
@@ -21,13 +21,31 @@ class NegociacaoController {
         );
 
         this._ordemAtual = '';
+
+        ConnectioFactory
+            .getConnection()
+            .then(connection => new NegociacaoDAO(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                    negociacoes.forEach(negociacao => 
+                        this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => this._mensagem.texto = erro);
     }
 
     adiciona (event) {
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._limpaFormulario();
+
+        ConnectioFactory.getConnection().then(connection => {
+            let negociacao = this._criaNegociacao();
+
+            new NegociacaoDAO(connection)
+                .adiciona(negociacao)
+                .then(() => {
+                    this._listaNegociacoes.adiciona(negociacao);
+                    this._mensagem.texto = 'Negociação adicionada com sucesso';
+                    this._limpaFormulario();
+                })
+            }).catch(erro => this._mensagem.texto = erro);
     }
 
     importaNegociacoes() {
@@ -40,16 +58,22 @@ class NegociacaoController {
     }
 
     apaga() {
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectioFactory.getConnection()
+        .then(connection => new NegociacaoDAO(connection))
+        .then(dao => dao.apagaTodos())
+        .then(mensagem => {
+            this._listaNegociacoes.esvazia();
+            this._mensagem.texto = mensagem;
+        })
+        .catch(erro => this._mensagem.texto = erro);
     }
 
     _criaNegociacao() {
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        )
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
     }
 
     _limpaFormulario() {
